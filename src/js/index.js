de5868ebe2019ebb1db1dff5f3fe3bb699a833b5
Anderson <eaglesquad86@gmail.com>
2020-02-13 12:43:35 +0800

    Search icon -iig gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,42 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
-// state
-/*
-Web app - ийн төлөв
-- Хайлтын query, үр дүн
-- Тухайн үзүүлж байгаа жор
-- Лайкласан жорууд
-- Захиалж байгаа жорын найрлагууд 
-*/
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
+
+/**
+ * Web app төлөв
+ * - Хайлтын query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкласан жорууд
+ * - Захиалж байгаа жорын найрлаганууд
+ */
+
 const state = {};
+
 const controlSearch = async () => {
-  // 1. вэбээс хайлтын түлхүүр үгийг гаргаж авна
-  const query = "pizza";
+  // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+  const query = searchView.getInput();
 
   if (query) {
-    // 2. Шинээр хайлтын обьектийг үүсгэж өгнө
+    // 2) Шинээр хайлтын обьектийг үүсгэж өгнө.
     state.search = new Search(query);
-    // 3. Хайлт хийхэд зориулж дэлгэцийн UI бэлтгэж өгнө
-    // 4. хайлтыг гүйцэтгэнэ.
+
+    // 3) Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ.
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
+
+    // 4) Хайлтыг гүйцэтгэнэ
     await state.search.doSearch();
-    // 5. хайлтын үр дүнг дэлгэцэнд хэвлэнэ.
-    console.log(state.search.result);
+
+    // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    clearLoader();
+    if (state.search.result === undefined) alert("Хайлтаар илэрцгүй...");
+    else searchView.renderRecipes(state.search.result);
   }
 };
-document.querySelector(".search").addEventListener("submit", e => {
-  e.preventDefault(); // submit хийгдсэний дараа автоматаар refresh хийгдэхийг болиулна
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
   controlSearch();
 });
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,6 +1,32 @@
-var superAdd = (a, b) => {
-  return a + b + 1000;
+import { elements } from "./base";
+// private function
+const renderRecipe = recipe => {
+  console.log(recipe);
+  const markup = `
+      <li>
+          <a class="results__link" href="#${recipe.recipe_id}">
+              <figure class="results__fig">
+                  <img src="${recipe.image_url}" alt="Test">
+              </figure>
+              <div class="results__data">
+                  <h4 class="results__name">${recipe.title}</h4>
+                  <p class="results__author">${recipe.publisher}</p>
+              </div>
+          </a>
+      </li>
+    `;
+  // ul рүүгээ нэмнэ
+  elements.searchResultList.insertAdjacentHTML("beforeend", markup);
 };
-export const add = (a, b) => a + b;
-export let multiply = (a, b) => a * b;
-export const id = 25;
+export const clearSearchQuery = () => {
+  elements.searchInput.value = "";
+};
+
+export const clearSearchResult = () => {
+  elements.searchResultList.innerHTML = "";
+};
+export const getInput = () => elements.searchInput.value;
+export const renderRecipes = recipes => {
+  recipes.forEach(renderRecipe);
+  // recipes.forEach( el=> renderRecipe(el));  ингэж бичиж ч болно
+};
