01680543fbec28a7acff7de58775d6bad336003c
Anderson <eaglesquad86@gmail.com>
2020-02-17 13:27:37 +0800

    Duuslaa:  Like - uudaa browser-n localStorage ruu hadgaldag bolloo

--- src/js/model/Like.js
+++ src/js/model/Like.js
@@ -1,11 +1,17 @@
 export default class Likes {
   constructor() {
-    this.likes = [];
+    this.readDataFromLocalStorage();
+
+    if (!this.likes) this.likes = [];
   }
 
   addLike(id, title, publisher, img) {
     const like = { id, title, publisher, img };
     this.likes.push(like);
+
+    // storage руу хадгална
+    this.saveDataToLocalStorage();
+
     return like;
   }
 
@@ -15,6 +21,9 @@ export default class Likes {
 
     // Уг индекс дээрх элементийг массиваас устгана
     this.likes.splice(index, 1);
+
+    // storage руу хадгална
+    this.saveDataToLocalStorage();
   }
 
   isLiked(id) {
@@ -25,5 +34,13 @@ export default class Likes {
 
   getNumberOfLikes() {
     return this.likes.length;
+  }
+
+  saveDataToLocalStorage() {
+    localStorage.setItem("likes", JSON.stringify(this.likes));
+  }
+
+  readDataFromLocalStorage() {
+    this.likes = JSON.parse(localStorage.getItem("likes"));
   }
 }
